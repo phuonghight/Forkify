1cc7a54e2ce14f0c0cd68c5601d5d357262e0e01
phuong <hoangphuong270703@gmail.com>
2023-03-06 22:52:02 +0700

    change KEY API

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const REC_PER_PAGE = 10;
-export const KEY = '7a439629-4b7f-4039-9d0b-ceddd7883291';
+export const KEY = '68d4f3c0-f4ec-4138-b886-0ea4bf691245';
